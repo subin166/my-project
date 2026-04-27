fb279de1c07633c4c2b0f2b49530a85ad4c1ebf9
EC2 Default User <ec2-user@ip-172-31-5-110.ap-southeast-2.compute.internal>
2026-04-27 19:43:43 +0000

    docker image and rebuild script

---
+++ my-node-app/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Node.js app running behind Apache!");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
