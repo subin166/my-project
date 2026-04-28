3974c06aaee537bcef81383a4dff626f2fd827e6
EC2 Default User <ec2-user@ip-172-31-5-110.ap-southeast-2.compute.internal>
2026-04-28 12:26:50 +0000

    security group fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,9 +46,9 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
